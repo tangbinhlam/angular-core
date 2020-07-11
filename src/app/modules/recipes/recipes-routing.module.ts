143dc4d7925e8cf13729ccf49a1532022d104e51
osalam <tangbinhlam@gmail.com>
2020-07-11 11:56:19 -0500

    [Recipe] - modify recipe-detail

--- src/app/modules/recipes/recipes-routing.module.ts
+++ src/app/modules/recipes/recipes-routing.module.ts
@@ -1,12 +1,12 @@
 import {RouterModule, Routes} from '@angular/router';
-import {RecipeListComponent} from './recipe-list/recipe-list.component';
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {NgModule} from '@angular/core';
+import {RecipesComponent} from './recipes.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: RecipeListComponent
+    component: RecipesComponent
   },
   {
     path: 'edit',
